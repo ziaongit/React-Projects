515a63e810510f771f3d8005d7bdbc4f84376eb4
Zia <zia_gt@yahoo.com>
2017-08-23 22:34:38 +0200

    some fixing

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,7 +5,32 @@ class  App extends Component {
     
     render() {
         return (
-            <div>Hello World</div>
+                <header id="showcase">
+                    <h1>Tic Tac Toe Game</h1>
+                    <div class="game-container">
+                        <div class="inner-container">
+                            <section class="section">
+                                <div class="box"></div>
+                                <div class="box"></div>
+                                <div class="box"></div>
+                            </section>
+                            <section class="section">
+                                <div class="box"></div>
+                                <div class="box"></div>
+                                <div class="box"></div>
+                            </section>
+                            <section class="section">
+                                <div class="box"></div>
+                                <div class="box"></div>
+                                <div class="box"></div>
+                            </section>
+                        </div>
+                    </div>
+                    <div class="winner"></div>
+                    <div class="button">
+                        <button id="restart">Restart</button>
+                    </div>
+                </header>
         );
     }
 }
