ab25a3fba53de06e3126fe4fbdac334507b96249
Zia <zia_gt@yahoo.com>
2017-08-30 00:27:51 +0200

    markdown previewer logic done

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormControl from 'react-bootstrap/lib/FormControl'
+
+var marked = require('marked');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {markdown: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({markdown: event.target.value});
+  }
+
   render() {
+    let { markdown } = this.state;
     return (
-      <div className="App">
+      <div className="App container-fluid">
         <h1>Markdown Previewer</h1>
+        <div className="col-md-6">
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel> Enter Markdown</ControlLabel>
+            <FormControl
+                componentClass="textarea"
+                placeholder="Enter Markdown"
+                 defaultValue={this.state.value}
+                onChange={this.handleChange}>
+          </FormControl>
+          </FormGroup>
+        </div>
+        <div className="col-md-6">
+          <div dangerouslySetInnerHTML={{__html: marked(markdown)}}></div>
+        </div>
       </div>
     );
   }
