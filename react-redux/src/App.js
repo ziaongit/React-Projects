866983feb914f911a8c0938600020210b78bb07c
Predicare\zia <zia.canic@predicare.se>
2018-03-17 19:03:43 +0100

    React Redux example

---
+++ react-redux/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <PostForm />
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default App;
